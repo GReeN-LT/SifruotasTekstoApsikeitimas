a3905c7838b750f00b69d5903fe90659dd5d9af0
GReeN-LT <mindaugas.laboga23@gmail.com>
2018-04-26 10:58:26 +0300

    Add project files.

---
+++ Klientas/Klientas.cs
@@ -0,0 +1,58 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Klientas
+{
+    public partial class Klientas : Form
+    {
+        public Klientas()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpClient client;
+
+
+        public void Klientas_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += Client_DataReceived;
+        }
+
+        public void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                txtStatus.Text += e.MessageString;
+            });
+        }
+
+        public void btnConnect_Click_1(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            //client.Connect(txtHost.Text, Convert.ToInt32(txtPort.Text));
+            client = new SimpleTcpClient().Connect("127.0.0.1", 8910);
+        }
+
+        public void btnSend_Click_1(object sender, EventArgs e)
+        {
+            client.WriteLineAndGetReply(txtMessage.Text, TimeSpan.FromSeconds(1));
+        }
+
+       
+
+        
+
+        
+
+        
+    }
+}
---
+++ SifruotasTekstoApsikeitimas/Serveris.cs
@@ -0,0 +1,60 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SifruotasTekstoApsikeitimas
+{
+    public partial class Serveris : Form
+    {
+        public Serveris()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer server;
+
+        public void Serveris_Load(object sender, EventArgs e)
+        {
+            
+            server = new SimpleTcpServer();
+            server.Delimiter = 0x13;//enter
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+        }
+
+        public void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                txtStatus.Text += e.MessageString;
+                e.ReplyLine(string.Format("You said: {0}", e.MessageString));
+            });
+            
+        }
+
+        public void btnStart_Click_1(object sender, EventArgs e)
+        {
+            txtStatus.Text += "Server starting...";
+            System.Net.IPAddress ip = System.Net.IPAddress.Parse(txtHost.Text);
+            server.Start(ip, Convert.ToInt32(8910));
+        }
+
+        
+
+        public void btnStop_Click_1(object sender, EventArgs e)
+        {
+            if (server.IsStarted)
+                server.Stop();
+        }
+
+        
+    }
+}
